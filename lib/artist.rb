108d6edcf0b6428e805f33607126f056e90fc7a0
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-25 09:09:09 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,48 @@
+class Artist
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+    save
+  end
+  
+  def self.all
+   @@all
+  end
+  
+  def add_song(song)
+    song.artist = self 
+  end
+  
+  def songs
+   Song.all.select {|song| song.artist == self} 
+  end
+  
+  def self.find_or_create_by_name(name)
+   if self.find_by_name(name) 
+   self.find_by_name(name) 
+   else 
+   self.create_by_name(name)
+   end
+  end
+   
+   def self.find_by_name(name)
+     @@all.detect {|artist| artist.name == name}
+  end
+   
+   def self.create_by_name(name)
+     self.new(name).save
+     self.new(name)
+  end
+  
+    def save
+    @@all << self
+    end
+  
+  def print_songs
+    songs.each {|s| puts s.name}
+  end
+  
+end
